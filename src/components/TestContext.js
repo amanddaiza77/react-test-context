c5d4b1a2d881f2114d7c0a54effa310d7e2f6eb6
amanddaiza77 <suksanandamza@gmail.com>
2023-10-16 03:25:01 +0700

    test

--- src/components/TestContext.js
+++ src/components/TestContext.js
@@ -5,7 +5,7 @@ import { DataContext } from '../contexts/DataContext';
 const ExampleComponent = () => {
     const { data, updateData } = useContext(DataContext);
 
-    // ทำอะไรสักอย่างกับ data
+    // ทำอะไรสักอย่างกับ datas
 
     return (
         data // รีเทิร์น JSX ที่ใช้ data
